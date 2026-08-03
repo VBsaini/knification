f5470844025024c285b37ce6744cc6b6da99914e
VBsaini <vbsaini2004@gmail.com>
2026-08-03 16:55:52 +0530

    ADD: collision in enemies

--- Scripts/enemy_base.gd
+++ Scripts/enemy_base.gd
@@ -5,11 +5,17 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 
+func _ready() -> void:
+	$Detection.body_entered.connect(player_detected)
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
 	
-
 	move_and_slide()
+
+
+func player_detected(body):
+	print(body.name)
+	pass
